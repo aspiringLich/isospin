3cc4634ee9450ea05fbda1b405b8bb6b765aee58
aspiringLich <48413902+aspiringLich@users.noreply.github.com>
2022-10-18 18:19:08 -0400

    scolling LCD & other changes

---
+++ header.js
@@ -0,0 +1,57 @@
+// type title
+let title = document.querySelector("#lcd-matrix h1");
+let transitions = `
+
+
+
+&lt
+&ltI
+&ltIS
+&ltISO
+&ltISOS
+&ltISOSO
+&ltISOSOP
+&ltISOSOPN
+&ltISOSOPN
+&ltISOSOPN
+&ltISOSOPN
+&ltISOSOP
+&ltISOSO
+&ltISOS
+&ltISOS
+&ltISOS
+&ltISOSP
+&ltISOSPI
+&ltISOSPIN
+&ltISOSPIN&gt
+&ltISOSPIN&gt
+&ltISOSPIN&gt
+`.split("\n");
+
+let delay = 150;
+let variance = 0.3;
+function type_title(count) {
+    if (count == transitions.length - 1) return;
+
+    title.innerHTML = transitions[count];
+    setTimeout(type_title, Math.random() * delay * variance + delay * (1 - variance), count + 1)
+}
+type_title(0);
+
+
+// scrolling text
+let scrolling = document.querySelectorAll("#lcd-matrix p");
+
+let cnt = 0;
+function change_text(element) {
+    cnt += 1;
+    element.innerHTML =
+        [cnt, "Fizz", "Buzz", "FizzBuzz"][(cnt % 3 == 0 ? 1 : 0) + ((cnt % 5 == 0 ? 1 : 0) * 2)]
+        ;
+}
+
+scrolling.forEach((x) => {
+    x.addEventListener("animationstart", () => { change_text(x) });
+    x.addEventListener("animationiteration", () => { change_text(x) });
+})
+
