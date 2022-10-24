11ac6247e0fb1f952c3e2cdbdc4041785a819616
aspiringLich <48413902+aspiringLich@users.noreply.github.com>
2022-10-23 23:38:49 -0400

    reparse md file on modification

---
+++ src/data/markdown/isospin.md
@@ -0,0 +1,5 @@
+# Isospin
+
+Hello, yes this is in fact the website you are looking at currently. That's cool i guess.
+
+uhhhhh yeah. yep. mhm. yep
--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -1,5 +1,5 @@
 use lazy_static::lazy_static;
-use std::{collections::HashMap, fs, sync::Mutex};
+use std::{cmp::Ordering, collections::HashMap, fs, sync::Mutex};
 
 use crate::{config, Server};
 use afire::{Content, Response};
@@ -14,6 +14,7 @@ pub fn attach(server: &mut Server) {
 // returns the coorosponding markdown file from MARKDOWN_DIR as html
 // first checks if its already parsed a markdown file by the same name
 pub fn get_markdown(path: &String) -> Response {
+    // options
     let options: ComrakOptions = ComrakOptions {
         render: ComrakRenderOptions {
             unsafe_: true,
@@ -22,20 +23,54 @@ pub fn get_markdown(path: &String) -> Response {
         ..Default::default()
     };
 
-    let path_html = path.clone().split(".").next().unwrap().to_owned() + ".html";
+    // get the full paths
+    let mut path_html = path.clone().split(".").next().unwrap().to_owned() + ".html";
+    path_html = format!("{}{}", *config::MARKDOWN_OUT_DIR, path_html);
+    let path_md = format!("{}{}", *config::MARKDOWN_IN_DIR, path);
 
-    if let Ok(string) = fs::read_to_string(format!("{}{}", *config::MARKDOWN_OUT_DIR, path_html)) {
-        return Response::new().text(string).content(Content::HTML);
+    // make sure that the md file isnt more recent, cuz then we need to rebuild
+    let mut last_modified_html = None;
+    if let Ok(metadata) = fs::metadata(&path_html) {
+        last_modified_html = Some(metadata.modified())
+    }
+    let mut last_modified_md = None;
+    if let Ok(metadata) = fs::metadata(&path_md) {
+        last_modified_md = Some(metadata.modified())
     }
 
-    let f = fs::read_to_string(format!("{}{}", *config::MARKDOWN_IN_DIR, path));
+    // dbg!(&last_modified_html, &last_modified_md);
+    let rebuild =
+        if let (Some(Ok(mod_html)), Some(Ok(mod_md))) = (last_modified_html, last_modified_md) {
+            // if the md has been modified more recently than the html we need to rebuild anyway
+            // dbg!(&mod_html, &mod_md);
+            // dbg!(mod_md.cmp(&mod_html));
+            mod_md.cmp(&mod_html) == Ordering::Greater
+        } else {
+            false
+        };
+
+    if !rebuild {
+        // if there is already a html file available then just read from that
+        if let Ok(string) = fs::read_to_string(&path_html) {
+            return Response::new().text(string).content(Content::HTML);
+        }
+    }
+
+    // ok so read and parse the md file
+    let f = fs::read_to_string(&path_md);
     if let Ok(string) = f {
         let html = markdown_to_html(&string, &options);
-        match fs::write(format!("{}{}", *config::MARKDOWN_OUT_DIR, path_html), &html) {
+        // modified html: just add some stuff so it uses a style sheet and does some stuff
+        let mhtml = format!(
+            "<!DOCTYPE html><head><link rel=\"stylesheet\" href=\"text.css\"></head><body>{}</body>",
+            html
+        );
+        // write it out!
+        match fs::write(&path_html, &mhtml) {
             Ok(_) => {}
             Err(err) => return config::ERROR(&err),
         }
-        Response::new().text(html).content(Content::HTML)
+        Response::new().text(mhtml).content(Content::HTML)
     } else {
         config::ERROR(&f.err().unwrap())
     }
