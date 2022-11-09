ebd60f08907057623a4fbff85159adba6b6064e2
aspiringLich <48413902+aspiringLich@users.noreply.github.com>
2022-11-09 17:29:06 -0500

    Revert "more baking html!"
    This reverts commit 67099e30c534b6db6d6c850c714430acf26cc67f.

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,6 @@
-#![feature(default_free_fn)]
-
 use afire::{extension::ServeStatic, prelude::*};
 
 mod config;
-mod file;
 mod routes;
 mod setup;
 #[macro_use]
--- src/routes/md.rs
+++ src/routes/md.rs
@@ -1,8 +1,8 @@
 use afire::prelude::*;
 use comrak::{markdown_to_html, ComrakOptions, ComrakRenderOptions};
-use std::{cmp::Ordering, default::default, fs};
+use std::{cmp::Ordering, fs};
 
-use crate::{config, file::*};
+use crate::config;
 
 use anyhow::Result;
 
@@ -32,21 +32,41 @@ fn reparse_and_get_html(path: String) -> String {
     let options: ComrakOptions = ComrakOptions {
         render: ComrakRenderOptions {
             unsafe_: true,
-            ..default()
+            ..Default::default()
         },
-        ..default()
+        ..Default::default()
     };
 
     // get the full paths
     // dbg!(&path_html);
-    let path_html = path.strip_filetype() + ".html";
-    let path_html = format!("{}{}", &*config::MARKDOWN_OUT_DIR, path_html);
-
+    let mut path_html = path.clone().split(".").next().unwrap().to_owned() + ".html";
+    path_html = format!("{}{}", &*config::MARKDOWN_OUT_DIR, path_html);
     let path_md = format!("{}{}", &*config::MARKDOWN_IN_DIR, path);
 
-    if need_rebuild(&path_html, &path_md) {
+    // make sure that the md file isnt more recent, cuz then we need to rebuild
+    let mut last_modified_html = None;
+    if let Ok(metadata) = fs::metadata(&path_html) {
+        last_modified_html = Some(metadata.modified())
+    }
+    let mut last_modified_md = None;
+    if let Ok(metadata) = fs::metadata(&path_md) {
+        last_modified_md = Some(metadata.modified())
+    }
+
+    // dbg!(&last_modified_html, &last_modified_md);
+    let rebuild =
+        if let (Some(Ok(mod_html)), Some(Ok(mod_md))) = (&last_modified_html, &last_modified_md) {
+            // if the md has been modified more recently than the html we need to rebuild anyway
+            // dbg!(&mod_html, &mod_md);
+            // dbg!(mod_md.cmp(&mod_html));
+            mod_md.cmp(&mod_html) == Ordering::Greater
+        } else {
+            last_modified_html.is_none()
+        };
+
+    if rebuild {
         // ok so read and parse the md file
-        let string = fs::read_to_string(&path_md).expect("md file should be there");
+        let string = fs::read_to_string(&path_md).expect("couldnt find md file");
         let html = format!(
             "<div class=\"markdown-body\">{}</div>",
             markdown_to_html(&string, &options)
@@ -54,7 +74,7 @@ fn reparse_and_get_html(path: String) -> String {
 
         // write it out!
         // dbg!(&path_html);
-        fs::write(&path_html, &html).expect("should be able to write the parsed md successfully");
+        fs::write(&path_html, &html).expect("failed to write out parsed md");
     }
 
     path_html
