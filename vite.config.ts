ef394c12aecd71bcd66c2c18e583c9e1cfb6e1f3
Brandon Li <sirbrandonthenerd@gmail.com>
2023-10-24 20:27:16 -0400

    config

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,14 @@
-import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import { sveltekit } from "@sveltejs/kit/vite";
+import { defineConfig } from "vite";
+import path from "path";
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	resolve: {
+		alias: {
+			$src: path.resolve("./src"),
+			$lib: path.resolve("./src/lib"),
+			$assets: path.resolve(`./`),
+		},
+	},
 });
