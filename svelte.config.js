91d86fb38cdfe6030a99df9cd2a5eca5ea0e06a7
Brandon Li <sirbrandonthenerd@gmail.com>
2023-12-09 14:51:07 -0500

    config tweaks

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,18 @@
-import adapter from '@sveltejs/adapter-auto';
-import { vitePreprocess } from '@sveltejs/kit/vite';
+import adapter from "@sveltejs/adapter-auto";
+import preprocess from "svelte-preprocess";
+import nesting from "postcss-nesting";
+import tailwind from "tailwindcss";
+import autoprefixer from "autoprefixer";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
-	preprocess: vitePreprocess(),
+	preprocess: preprocess({
+		postcss: {
+			plugins: [nesting(), tailwind(), autoprefixer()],
+		},
+	}),
 
 	kit: {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
