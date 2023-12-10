0d5daabd379b8da366fdc2d7b939bbe3703d5aa1
Brandon Li <sirbrandonthenerd@gmail.com>
2023-12-10 18:02:08 -0500

    figured out a clean fix

--- src/lib/drag.ts
+++ src/lib/drag.ts
@@ -10,21 +10,18 @@ export type DragOptions = {
 };
 
 export const drag = (node: HTMLElement, options: DragOptions) => {
-	// console.log("drag");
 	const body_style = document.body.style;
 
 	let bounding_element: HTMLElement;
 
 	let bounding_rect: DOMRect;
 	let bounding_observer = new ResizeObserver(() => {
-		// console.log("bounding resize");
 		bounding_rect = bounding_element.getBoundingClientRect();
-		setTranslate(final_x, final_y, true);
+		readjust();
 	});
 
 	let node_rect: DOMRect = node.getBoundingClientRect();
 	let node_observer = new ResizeObserver(() => {
-		// console.log("node resize");
 		node_rect = node.getBoundingClientRect();
 	});
 	node_observer.observe(node);
@@ -36,69 +33,52 @@ export const drag = (node: HTMLElement, options: DragOptions) => {
 	let initial = true;
 	let offset_x = 0;
 	let offset_y = 0;
-	let final_x: number = 0;
-	let final_y: number = 0;
-	let acc_dx = 0;
-	let acc_dy = 0;
-	let p_width = node_rect.width;
-	let p_height = node_rect.height;
+	let final_cx: number = 0;
+	let final_cy: number = 0;
 
-	const setTranslate = (x: number, y: number, readjust: boolean = false) => {
-		// get x and y clamped to the bounding element
-		const x1 = x - bounding_rect.left;
-		const y1 = y - bounding_rect.top;
+	let percent_x: number = 0;
+	let percent_y: number = 0;
 
-		const xmax = bounding_rect.width - node_rect.width;
-		const ymax = bounding_rect.height - node_rect.height;
+	const setTranslate = (cx0: number, cy0: number) => {
+		const xmin = node_rect.width / 2;
+		const ymin = node_rect.height / 2;
+		const cxmax = bounding_rect.width - xmin;
+		const cymax = bounding_rect.height - ymin;
 
-		x = clamp(x1, 0, xmax);
-		y = clamp(y1, 0, ymax);
+		const cx = cx0 - bounding_rect.left;
+		const cy = cy0 - bounding_rect.top;
 
-		if (readjust) {
-			// adjust the position to account for the change in size
-			let dwidth = bounding_rect.width - p_width;
-			let dheight = bounding_rect.height - p_height;
-			x += dwidth * (x / xmax);
-			y += dheight * (y / ymax);
-			p_width = bounding_rect.width;
-			p_height = bounding_rect.height;
+		percent_x = (cx - xmin) / (cxmax - xmin);
+		percent_y = (cy - ymin) / (cymax - ymin);
 
-			// accumulate the difference between the clamped and unclamped values
-			const ax = x - x1;
-			const ay = y - y1;
-			acc_dx += ax;
-			acc_dy += ay;
+		final_cx = clamp(cx, xmin, cxmax);
+		final_cy = clamp(cy, ymin, cymax);
 
-			// adjust the final position by the accumulated difference
-			if (acc_dx < 0 && ax == 0) {
-				let dx = x - xmax;
-				x -= Math.max(dx, acc_dx);
-				acc_dx -= Math.max(dx, acc_dx);
-			}
-			if (acc_dy < 0 && ay == 0) {
-				let dy = y - ymax;
-				y -= Math.max(dy, acc_dy);
-				acc_dy -= Math.max(dy, acc_dy);
-			}
-		}
+		node.style.transform = `translate3d(${final_cx - xmin}px, ${final_cy - ymin}px, 0)`;
+	};
 
-		final_x = x;
-		final_y = y;
+	const readjust = () => {
+		const xmin = node_rect.width / 2;
+		const ymin = node_rect.height / 2;
+		const cxmax = bounding_rect.width - xmin;
+		const cymax = bounding_rect.height - ymin;
 
-		node.style.transform = `translate3d(${x}px, ${y}px, 0)`;
+		const cx = xmin + percent_x * (cxmax - xmin) - bounding_rect.left;
+		const cy = ymin + percent_y * (cymax - ymin) - bounding_rect.top;
+
+		final_cx = clamp(cx, xmin, cxmax);
+		final_cy = clamp(cy, ymin, cymax);
+
+		node.style.transform = `translate3d(${final_cx - xmin}px, ${final_cy - ymin}px, 0)`;
 	};
 
 	const onmousedown = (e: MouseEvent) => {
 		if (e.button !== 0 && e.button !== 1) return;
 
 		let { clientX, clientY } = e;
-		offset_x = clientX - final_x;
-		offset_y = clientY - final_y;
+		offset_x = clientX - final_cx;
+		offset_y = clientY - final_cy;
 
-		acc_dx = 0;
-		acc_dy = 0;
-
-		// console.log("mousedown");
 		e.preventDefault();
 
 		original_user_select_val = body_style.userSelect;
@@ -115,7 +95,6 @@ export const drag = (node: HTMLElement, options: DragOptions) => {
 	};
 
 	const onmouseup = (e: MouseEvent) => {
-		// console.log("mouseup");
 		body_style.userSelect = original_user_select_val;
 
 		document.removeEventListener("pointermove", onmousemove, false);
@@ -123,7 +102,6 @@ export const drag = (node: HTMLElement, options: DragOptions) => {
 	};
 
 	const update = (options: DragOptions) => {
-		// console.log("update");
 		const { handle_selector } = options;
 
 		// get the bounding element
@@ -141,11 +119,7 @@ export const drag = (node: HTMLElement, options: DragOptions) => {
 		handle.addEventListener("pointerdown", onmousedown, false);
 
 		// center the node
-		if (initial)
-			setTranslate(
-				bounding_rect.width / 4 - node_rect.width / 4,
-				bounding_rect.height / 4 - node_rect.height / 4
-			);
+		if (initial) setTranslate(bounding_rect.width / 2, bounding_rect.height / 2);
 		initial = false;
 	};
 	update(options);
