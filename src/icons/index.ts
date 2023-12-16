666dd9f096c11bcd96c323a8d6ce7cc9bdc0fff5
Brandon Li <sirbrandonthenerd@gmail.com>
2023-12-16 18:36:36 -0500

    icons??

---
+++ src/icons/index.ts
@@ -0,0 +1,18 @@
+export namespace Icon {
+	export enum file {
+		blank,
+		text,
+	}
+
+	export enum folder {
+		empty = 8,
+		full,
+	}
+
+	export enum application {
+		file_manager = 16,
+		minesweeper,
+		terminal,
+		settings,
+	}
+}
